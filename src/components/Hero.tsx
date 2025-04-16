f9ac285f5ba468025e32320baa991072df1b54ad
robinjo1776 <robinjo1776@gmail.com>
2025-04-16 16:57:13 +0000

    Hero section completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,55 @@
 import React from "react";
 import "../styles/Hero.css";
 import logo from "../../public/assets/images/logo-banner.png";
+import arrow from "../../public/assets/images/arrow_icon.png";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
 
 const Hero = () => {
   return (
     <section className="hero-section">
+      <div className="social-icons">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <FaYoutube />
+        </a>
+      </div>
+
       <div className="hero-content">
-        <p>WEB · DESIGN · E-COMMERCE · SEO</p>
+        <p className="heading-1">WEB · DESIGN · E-COMMERCE · SEO</p>
         <div className="logo">
-          <img src={logo} alt="Logo" className="img-logo"/>
+          <img src={logo} alt="Logo" className="img-logo" />
         </div>
         <p className="result">RESULT DRIVEN</p>
         <p>Affordable digital services for Canadians.</p>
 
-        {/* Button Container */}
         <div className="hero-buttons">
-          <button className="hero-button">GET STARTED</button>
-          <button className="hero-button secondary">OUR SERVICES</button>
+          <button className="hero-button">
+            GET STARTED
+            <img src={arrow} alt="Logo" className="arrow" />
+          </button>
+          <button className="hero-button secondary">
+            OUR SERVICES
+            <img src={arrow} alt="Logo" className="arrow" />
+          </button>
         </div>
       </div>
     </section>
